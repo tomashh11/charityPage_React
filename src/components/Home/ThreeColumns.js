bc92b7ebea65ca475404b339d2759ce3fdee63eb
Tomasz Wroński <tomashh11@gmail.com>
2020-04-14 13:44:01 +0200

    threeColumns done

--- src/components/Home/ThreeColumns.js
+++ src/components/Home/ThreeColumns.js
@@ -2,7 +2,25 @@ import React from 'react';
 
 const ThreeColumns = () => {
     return <>
-    <div id="about">Home Three Columns</div>
+        <div className="threeColumns container" id="about">
+            <div className="mainSection">
+                <div className="singleSection">
+                    <h2>10</h2>
+                    <h3>oddanych worków</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.</p>
+                </div>
+                <div className="singleSection">
+                    <h2>5</h2>
+                    <h3>wspartych organizacji</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.</p>
+                </div>
+                <div className="singleSection">
+                    <h2>7</h2>
+                    <h3>zorganizowanych zbiórek</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.</p>
+                </div>
+            </div>
+        </div>
     </>
 };
 
