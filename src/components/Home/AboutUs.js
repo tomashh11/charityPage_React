3a272f4e319c21ca3e848f72e9f937a4deba7ca4
Tomasz Wroński <tomashh11@gmail.com>
2020-04-14 16:33:03 +0200

    aboutUs done

--- src/components/Home/AboutUs.js
+++ src/components/Home/AboutUs.js
@@ -2,7 +2,15 @@ import React from 'react';
 
 const AboutUs = () => {
     return <>
-    <div id="aboutUs">About Us</div>
+        <div className="container mainAboutUs" id="aboutUs">
+            <div className="aboutUsDescription">
+                <h2>O nas</h2>
+                <div className="aboutUsLogo"></div>
+                <p>Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.</p>
+                <div className="aboutUsSign"></div>
+            </div>
+            <div className="aboutUsPhoto"></div>
+        </div>
     </>
 };
 
